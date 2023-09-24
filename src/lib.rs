5c9918382694c48e6f227acb335ec1517d3564b1
Jakub Arnold <darthdeus@gmail.com>
2023-09-24 23:53:11 +0200

    Fix git-version-macro + version bump

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! These macros do not depend on libgit, but simply uses the `git` binary directly.
 //! So you must have `git` installed somewhere in your `PATH`.
 
-pub use git_version_macro::git_version;
+pub use comfy_git_version_macro::git_version;
 
 /// Run `git describe` at compile time with custom flags.
 ///
